b0297c571f1b15724b7fbd17aefb1667b115a942
Philip James <pjj@philipjohnjames.com>
2016-04-17 19:50:50 -0700

    Error concatenating middleware classes

--- tintg/settings.py
+++ tintg/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'games',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,7 +61,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+)
 
 ROOT_URLCONF = 'tintg.urls'
 
